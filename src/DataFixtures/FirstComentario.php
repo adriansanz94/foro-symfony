d9dcf1d1269c8c14c8758700c9e814a65f786cc7
adriansanz94 <adriansanzperez.94@gmail.com>
2020-02-10 17:56:21 +0100

    funciona acceder a publicacion

--- src/DataFixtures/FirstComentario.php
+++ src/DataFixtures/FirstComentario.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Categoria;
 use App\Entity\Comentario;
 use App\Entity\Publicación;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -16,6 +17,13 @@ class FirstComentario extends Fixture
         $contenidos = ['Esto es lo maximo','Esto es lo peor','Ame estos momentos','Viva la vida','De mal a peor','Mi vida cambio','Prefiero morir','Hasta junio','Hasta septiembre','Necesito dormir mas','Pizza time','Nunca nunca jamas jamas','Aguacates'];
         $comentarios = ['Lo apoyo','Que va','Correcto','Ni loco','TT_TT',':D'];
         $repoCat = $manager->getRepository(Categoria::class);
+
+        $repoUser = $manager->getRepository(User::class);
+
+        $u = $repoUser->findOneBy([
+            'username' => 'jorge@asd.es'
+        ]);
+
         $minCat = min($repoCat->findAll())->getId();
         $maxCat = max($repoCat->findAll())->getId();
 
@@ -25,12 +33,13 @@ class FirstComentario extends Fixture
 
             $random = mt_rand($minCat,$maxCat);
             $c = $repoCat->find($random);
-            dump($c);
+           // dump($c);
 
             $p->setCategoria($c);
             $p->setContenido($contenido);
             $p->setTítulo($contenidos[mt_rand(0,count($contenidos)-1)]);
             $p->setFechaPublicación(new \DateTime("now"));
+            $p->setUsuario($u);
 
             for ($i = 0 ; $i < mt_rand(0,count($comentarios)-1) ; $i++){
                 $com = new Comentario();
@@ -38,6 +47,7 @@ class FirstComentario extends Fixture
                 $com->setFechaPublicación(new \DateTime('now'));
 
                 $com->setPublicación($p);
+                $com->setUsuario($u);
 
                 $manager->persist($com);
             }
